df67359fecfe48f18274ca9904867e6ba42377d9
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-07-30 13:20:20 +0100

    Styling the <Head />

--- src/Components/Head/Head.jsx
+++ src/Components/Head/Head.jsx
@@ -4,28 +4,32 @@ import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
+import useStyles from './style';
+
 const Head = () => {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        <Typograghy variant="h5" className={classes.title}>
+        <Typography variant="h5" className={classes.title}>
           Tourist Adviser
-        </Typograghy>
-        <Box display="flex">
+        </Typography>
+        <Box style={{ display: 'flex' }}>
           <Typography variant="h6" className={classes.title}>
             Find your path
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search for ..."
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Autocomplete>
+            <InputBase
+              placeholder="Search ..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
