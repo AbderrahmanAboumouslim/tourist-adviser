f3e4469a93381b2f670cc2bd25aeffd96aa65721
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-07-30 12:14:31 +0100

    Structuring files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Head from './Head/Head';
+import List from './List/List';
+import Map from './Map/Map';
+
 const App = () => {
   return <div>Tourist Adviser Application</div>;
 };
