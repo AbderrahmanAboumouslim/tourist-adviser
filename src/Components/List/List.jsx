f6a39490de90accf354ff7716451e22ed2b3d76b
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-07-31 11:30:43 +0100

    First part of <List />

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+import useStyles from './style';
 
 const List = () => {
-  return <div>List</div>;
+  const classes = useStyles();
+  const [type, setType] = useState('hotels');
+  const [rate, setRate] = useState('');
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Hotels, Restaurants and Attractions</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={e => setType(e.target.value)}>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="resturants">Resturants</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rate} onChange={e => setRate(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value="3">3.0+</MenuItem>
+          <MenuItem value="4">4.0+</MenuItem>
+          <MenuItem value="4.5">4.5+</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
