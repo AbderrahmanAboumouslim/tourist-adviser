b2d7630a476ae12b94eea6be8cbe7513064611a8
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-08-05 20:52:16 +0100

    adding the cuisine <Chip />

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -13,10 +13,13 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 
+import useStyles from './style';
+
 const altImg =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRfpZB0_3qGRT0vx7Jlw662goIgQc9en4esg&usqp=CAU';
 
 const Details = ({ place }) => {
+  const classes = useStyles();
   return (
     <Card elevation={6}>
       <CardMedia
@@ -47,6 +50,10 @@ const Details = ({ place }) => {
               {award.display_name}
             </Typography>
           </Box>
+        ))}
+
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
         ))}
       </CardContent>
     </Card>
